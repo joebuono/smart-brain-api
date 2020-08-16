2b997ade5d5785161bebeacbe05f0d863c525bc6
Joe Buono <j.buono7@yahoo.com>
2020-08-15 22:43:28 -0400

    fix

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : process.env.DATABASE_URL, 
+    connectionString : process.env.DATABASE_URL, 
     // This is the database on Heroku
     // It was localhost 127.0.0.1 (home) during development
     ssl: true
